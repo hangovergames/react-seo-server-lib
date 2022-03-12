53c9eb11bd838ca8c43e3b2ecb012037ed6de1b4
Jaakko Heusala <jhh@heusalagroup.fi>
2022-03-12 02:09:32 +0200

    Fixed references after refactoring

--- services/FileSystemService.ts
+++ services/FileSystemService.ts
@@ -1,7 +1,7 @@
-// Copyright (c) 2021. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
+// Copyright (c) 2021-2022. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
 
-import { FS } from "fs";
-import { LogService } from "../../../hg/core/LogService";
+import { readFile } from "fs";
+import { LogService } from "../../core/LogService";
 import ErrnoException = NodeJS.ErrnoException;
 
 const LOG = LogService.createLogger('FileSystemService');
@@ -11,7 +11,7 @@ export class FileSystemService {
     public static async readTextFile (fileName : string) : Promise<string> {
         LOG.debug(`Reading file: `, fileName);
         return await new Promise((resolve, reject) => {
-            FS.readFile(fileName, {encoding: 'utf8'}, (err: ErrnoException | null, data : string) => {
+            readFile(fileName, {encoding: 'utf8'}, (err: ErrnoException | null, data : string) => {
                 if ( err ) {
                     reject(err);
                 } else {
