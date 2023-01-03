7c41753b09b5fff9d560cbdc28473ffb9890a40f
Jaakko Heusala <jhh@heusalagroup.fi>
2023-01-03 11:12:44 +0200

    Update StaticReactAppService.test.tsx

--- services/StaticReactAppService.test.tsx
+++ services/StaticReactAppService.test.tsx
@@ -14,6 +14,13 @@ describe("StaticReactAppService", () => {
             expect(result).toBe(expectedResult);
         });
 
+        it("should throw an error if the url parameter is not a string", () => {
+            const url = 123; // This is not a string
+            const App = () => <div>Test</div>;
+
+            expect(() => StaticReactAppService.renderString(url, App)).toThrowError();
+        });
+        
     });
 
 });
